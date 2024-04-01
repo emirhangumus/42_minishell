08dadb56e379af2bf49a1fa88614b0bccfb1ff97
Emirhan Gumus <egumus@k1m09s01.42istanbul.com.tr>
2024-04-01 09:51:45 +0300

    Fix: signal sigint

--- signals.c
+++ signals.c
@@ -6,7 +6,7 @@
 /*   By: egumus <egumus@student.42istanbul.com.t    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/03 16:34:36 by egumus            #+#    #+#             */
-/*   Updated: 2024/04/01 09:26:44 by egumus           ###   ########.fr       */
+/*   Updated: 2024/04/01 09:51:12 by egumus           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,32 +14,33 @@
 
 int	g_qsignal;
 
-void	sigquit_handler(int num)
+void	coix(int sig)
 {
-	(void)num;
-	if (g_qsignal == 1)
-	{
-		write(1, "\033[2D", 4);
-		write(1, "  ", 2);
-		write(1, "\033[2D", 4);
-		ioctl(0, TIOCSTI);
-		g_qsignal = 0;
-	}
+	(void)sig;
+	rl_on_new_line();
+	printf("\033[K");
+	rl_redisplay();
 	g_qsignal = 1;
 }
 
-void	sigint_handler(int sig)
+void	ctrl_c(int sig)
 {
 	(void)sig;
-	write(1, "\033[A", 3);
-	ioctl(0, TIOCSTI, "\n");
-	g_qsignal = 1;
+	if (!g_qsignal)
+	{
+		printf("\n");
+		rl_replace_line("", 0);
+		rl_on_new_line();
+		rl_redisplay();
+	}
+	else
+		printf("\n");
 }
 
 
 void	ft_signals(void)
 {
 
-	signal(SIGINT, sigint_handler);
-	signal(SIGQUIT, sigquit_handler);
+	signal(SIGINT, ctrl_c);
+	signal(SIGQUIT, coix);
 }
