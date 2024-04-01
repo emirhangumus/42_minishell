683175417685762fd02f13e3af555767cac0a1e5
BurakKaya1299 <burakkaya_2004@hotmail.com>
2024-04-01 08:06:25 +0300

    Signals test 1

--- signals.c
+++ signals.c
@@ -3,31 +3,43 @@
 /*                                                        :::      ::::::::   */
 /*   signals.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: egumus <egumus@student.42istanbul.com.t    +#+  +:+       +#+        */
+/*   By: burkaya <burkaya@student.42istanbul.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/03 16:34:36 by egumus            #+#    #+#             */
-/*   Updated: 2024/03/31 22:31:01 by egumus           ###   ########.fr       */
+/*   Updated: 2024/04/01 07:59:57 by burkaya          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static void	ifsendsigquit(int signal)
+int	g_qsignal;
+
+void	coix(int sig)
 {
-	(void)signal;
+	(void)sig;
+	rl_on_new_line();
+	printf("\033[K");
 	rl_redisplay();
-	return ;
+	g_qsignal = 1;
 }
 
-static void	ifsendsigint(int signal)
+void	ctrl_c(int sig)
 {
-	(void)signal;
-	write(1, "\033[A", 4);
-	ioctl(0, TIOCSTI, "\n");
+	(void)sig;
+	if (!g_qsignal)
+	{
+		printf("\n");
+		rl_replace_line("", 0);
+		rl_on_new_line();
+		rl_redisplay();
+	}
+	else
+		printf("\n");
 }
+
 
 void	ft_signals(void)
 {
-	signal(SIGINT, ifsendsigint);
-	signal(SIGQUIT, ifsendsigquit);
+	signal(SIGINT, ctrl_c);
+	signal(SIGQUIT, coix);
 }
